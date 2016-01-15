2bfd5404dd552c90ca56bb6b95909d36b6060511
WillJHaggard <willjhaggard@gmail.com>
2016-01-14 23:23:08 -0500

    Added bach file for running jake with pc in proj

--- jakefile.js
+++ jakefile.js
@@ -1,5 +1,5 @@
 /*global desc, task, jake, fail, complete*/
-
+(function() {
 "use strict";
 
 task("default", ["lint"]);
@@ -13,9 +13,29 @@ task("lint", [], function() {
     files.exclude("node_modules");
     files.exclude("build");
 
-    var options = {
-        node: true
-    };
 
-    lint.validateFileList(files.toArray(), options, {});
+    lint.validateFileList(files.toArray(), nodeLintOptions, {});
 });
+
+
+function nodeLintOptions() {
+        return {
+            bitwise: true,
+            curly: false,
+            eqeqeq: true,
+            forin: true,
+            immed: true,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            noempty: true,
+            nonew: true,
+            regexp: true,
+            undef: true,
+            strict: true,
+            trailing: true,
+            node: true
+        };
+
+}
+})();
